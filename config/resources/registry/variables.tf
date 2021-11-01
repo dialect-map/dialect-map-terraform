2f926393de57ffa07a138a6d4b9365f3b890ff94
Sinclert Pérez <Sinclert@hotmail.com>
2021-11-01 16:16:20 -0400

    Migrate `registry` terraform resources

---
+++ config/resources/registry/variables.tf
@@ -0,0 +1,11 @@
+# List of empty values to be filled once the module is imported
+
+variable "project_id" {
+  type        = string
+  description = "GCP Project ID for the Dialect Map resources"
+}
+
+variable "project_location" {
+  type        = string
+  description = "GCP Project location for the Dialect Map resources"
+}
